235e9ed2c2c57c33aef5bdfbe7d057d94167b6db
Claudia Silvestre <cla.silvestremu@alum.uca.es>
2023-01-01 14:53:32 +0100

    Home updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,16 @@ const Home = () => {
     return (
         <Layout>
             <div>
-                <img className="responsive" src="/img/wall.jpg" alt="Muro morado" />
+                <img className="responsive" src="/img/neon.jpg" alt="Cartel japonés de neón" />
 
                 <div className="group">
+                    {/* Se cambia por un bucle for para las prendas */}
                     <div className="clothes">
-                        <img className="responsive img" src="/img/foto1.png" alt="Muro morado" />
-                        <a>Camiseta oversize</a>
+                        <img className="responsive" src="/img/foto1.png" alt="Camiseta oversize" />
+                        <h3>Camiseta oversize</h3>
                         <span className="prize">€20,00</span>
                     </div>
+                    {/* Termina bucle */}
                 </div>
             </div>
         </Layout>
