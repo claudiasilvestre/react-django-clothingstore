e9970ea879541bf57fe2bb9fc22ce7ba6f5c4658
Claudia Silvestre <cla.silvestremu@alum.uca.es>
2023-01-02 18:45:01 +0100

    Product page created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home'
+import Product from './components/Product'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/product' element={<Product />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Product.jsx
@@ -0,0 +1,45 @@
+import Layout from '../hocs/Layout'
+import { AiOutlineHeart } from "react-icons/ai";
+import Select from 'react-select'
+
+const options = [
+    { value: 'xs', label: 'XS' },
+    { value: 's', label: 'S' },
+    { value: 'm', label: 'M' },
+    { value: 'l', label: 'L' },
+    { value: 'xl', label: 'XL' }
+]
+
+const Product = () => {
+    return (
+        <Layout>
+            <div className="product">
+                <div className="p-20">
+                    <img className="responsive" src="/img/foto2.png" alt="Camiseta oversize" />
+                </div>
+
+                <div className="p-20 product-description">
+                    <h1>Camiseta oversize</h1>
+                    <span className="prize f-l">€20,00</span>
+                    <div className="pt-20">
+                        <Select className="sizes-select" placeholder="Talla" options={options} />
+                        <div className="options-group">
+                            <button className="cart-button mr-10 responsive">
+                                Añadir a mi cesta
+                            </button>
+                            <button className="heart-button ml-10">
+                                <AiOutlineHeart size="25px" />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="pt-20">
+                        <h3>Descripción completa</h3>
+                        <p>Camiseta básica oversize de manga corta, con cuello redondo y confeccionadas en tejido 100% algodón.</p>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Product
