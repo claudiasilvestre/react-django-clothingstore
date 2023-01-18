0187232dfbb0ccb35859590854eac6aa4c7be6b7
Claudia Silvestre <cla.silvestremu@alum.uca.es>
2023-01-18 20:23:59 +0100

    Starting with axios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,34 @@
 import Layout from '../hocs/Layout'
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            const url = '/api/products';
+            const result = await axios.get(url);
+
+            setProducts(result.data.products);
+        }
+        getProducts();
+
+    }, []);
+
     return (
         <Layout>
             <div>
                 <img className="responsive" src="/media/neon.jpg" alt="Cartel japonés de neón" />
 
                 <div className="group">
-                    {/* Se cambia por un bucle for para las prendas */}
-                    <div className="clothes">
-                        <img className="responsive" src="/media/foto1.png" alt="Camiseta oversize" />
-                        <h3>Camiseta oversize</h3>
-                        <span className="prize">€20,00</span>
-                    </div>
-                    {/* Termina bucle */}
+                    {products.map(product => (
+                        <div className="clothes">
+                            <img className="responsive" src="/media/foto1.png" alt={product.name} />
+                            <h3>{product.name}</h3>
+                            <span className="prize">€{product.price}</span>
+                        </div>
+                    ))}
                 </div>
             </div>
         </Layout>
