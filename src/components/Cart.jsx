cd5396ebdf63cb94fe15cb67df3155314f9bee7d
Claudia <cla.silvestremu@alum.uca.es>
2023-03-12 15:12:30 +0100

    Cart front almost completed

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import classNames from 'classnames';
 import { Link } from "react-router-dom";
 import { AiOutlineClose, AiOutlineMinus, AiOutlinePlus, AiOutlineDelete } from "react-icons/ai";
 
-const Cart = () => {
+const Cart = props => {
     const [products, setProducts] = useState([]);
 
     const count = 111;
@@ -17,37 +18,39 @@ const Cart = () => {
       }, []);
 
     return (
-        <div className="sidebar">
-            <div className="flex-row-space pb-20">
-                <h1>Tu carrito</h1>
-                <button className="close-button"><AiOutlineClose size="20px" /></button>
-            </div>
+        <div className={props.isOpen ? "sidebarMask" : ""}>
+            <div className={classNames("sidebar", {"sidebarShow": props.isOpen})}>
+                <div className="flex-row-space pb-20">
+                    <h1>Tu carrito</h1>
+                    <button onClick={props.onChangeIsOpen} className="close-button"><AiOutlineClose size="20px" /></button>
+                </div>
 
-            {products.map(product => (
-                <div className="flex-row pb-20">
-                    <Link to={`/products/${product.id}`}>
-                        <img src="/media/foto1.png" alt={product.name} width="100" height="150" />
-                    </Link>
+                {products.map(product => (
+                    <div className="flex-row pb-20">
+                        <Link to={`/products/${product.id}`}>
+                            <img src="/media/foto1.png" alt={product.name} width="100" height="150" />
+                        </Link>
 
-                    <div className="flex-column-space">
-                        <div className="flex-column">
-                            <Link to={`/products/${product.id}`}>
-                                <a>{product.name}</a>
-                            </Link>
-                            <span className="price">S</span>
-                            <span className="price">€{product.price}</span>
-                        </div>
-                        <div className="options-group">
-                            <div className="count-box mb-5">
-                                <button className="count-button"><AiOutlineMinus size="13px" /></button>
-                                <input type="text" defaultValue={count} className="cart-input" />
-                                <button className="count-button"><AiOutlinePlus size="13px" /></button>
+                        <div className="flex-column-space">
+                            <div className="flex-column">
+                                <Link to={`/products/${product.id}`}>
+                                    <a>{product.name}</a>
+                                </Link>
+                                <span className="price">S</span>
+                                <span className="price">€{product.price}</span>
                             </div>
-                            <a href="#"><AiOutlineDelete size="20px" /></a>
+                            <div className="options-group">
+                                <div className="count-box mb-5">
+                                    <button className="count-button"><AiOutlineMinus size="13px" /></button>
+                                    <input type="text" defaultValue={count} className="cart-input" />
+                                    <button className="count-button"><AiOutlinePlus size="13px" /></button>
+                                </div>
+                                <a href="#"><AiOutlineDelete size="20px" /></a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import { AiOutlineUser, AiOutlineShopping } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import Alert from '../alert'
 
+import Alert from '../alert'
 import Cart from '../Cart'
 
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openCart = () => {
+        setIsOpen(!isOpen);
+    }
 
     return (
         <>
@@ -12,12 +18,13 @@ export const Navbar = () => {
                 <Link to="/"><h1>CS</h1></Link>
                 <div>
                     <Link to="/account/login"><AiOutlineUser size="25px" /></Link>
-                    <a href="#"><AiOutlineShopping className="nav-m" size="25px" /></a>
+                    <button onClick={openCart}><AiOutlineShopping className="nav-m" size="25px" /></button>
                 </div>
             </header>
+
             <Alert />
 
-            <Cart />
+            <Cart isOpen={isOpen} onChangeIsOpen={openCart} />
         </>
     )
 }
